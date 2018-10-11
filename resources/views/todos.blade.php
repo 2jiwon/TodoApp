b0440b92b3ceb152c8ecbeff5a0025fa5685a3a7
2jiwon <ljwjulian@gmail.com>
2018-10-11 11:35:00 +0900

    Add a token to the form for a request.

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -5,6 +5,7 @@
 <div class="row justify-content-center">
   <div class="col-lg-6 col-lg-offset-3">
     <form action="/create/todo" method="post">
+      {{ csrf_field() }}
       <input class="form-control input-lg" type="text" name="todo" placeholder="Create a new list">
     </form>
   </div>
