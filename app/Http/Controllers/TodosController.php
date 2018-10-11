2e0f379e03936dfe769917aff0ad329576d024f7
2jiwon <ljwjulian@gmail.com>
2018-10-11 12:10:00 +0900

    Modify the store method.

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -14,6 +14,11 @@ class TodosController extends Controller
     }
 
     public function store(Request $request) {
-      dd($request);
+      $todo = new Todo;
+
+      $todo->todo = $request->todo;
+      $todo->save();
+
+      return redirect()->back();
     }
 }
