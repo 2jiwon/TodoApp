963aa41691b69f00bbabe6b9fccdb25941e8067e
2jiwon <ljwjulian@gmail.com>
2018-10-11 09:10:00 +0900

    Add Todo class and pass the todos values to the view.

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodosController extends Controller
 {
     public function index () {
-      return view('todos');
+      $todos = Todo::all();
+
+      return view('todos')->with('todos', $todos);
     }
 }
