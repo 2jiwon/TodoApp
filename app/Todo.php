7435584f1e3a437e3f7fb3c0572d9e038bfdf023
2jiwon <ljwjulian@gmail.com>
2018-10-12 10:50:00 +0900

    Add 'SoftDelete' to the Todo model and migrate.

--- app/Todo.php
+++ app/Todo.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    use SoftDeletes;
+    protected $dates = ['deleted_at'];
 }
