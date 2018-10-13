7c2e70ba6753bbab959c3824939f6eb5995701c8
2jiwon <ljwjulian@gmail.com>
2018-10-13 11:10:00 +0900

    Add session display to the view.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,6 +30,15 @@
                 </div>
             @endif
 
+            @if (Session::has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+              {{ Session::get('success') }}
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            @endif
+
             <div class="content">
                 <div class="pt-5">
                 @yield('content')          
